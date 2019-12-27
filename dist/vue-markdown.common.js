c0f1b35c7b22df1eb668e95f9131b2e3b6b02990
jlykins <50977426+jlykins@users.noreply.github.com>
2019-12-27 05:14:42 +0000

    Update vue-markdown.common.js

--- dist/vue-markdown.common.js
+++ dist/vue-markdown.common.js
@@ -322,7 +322,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    outHtml = this.postrender(outHtml);
 
 	    this.$emit('rendered', outHtml);
-	    return createElement('div', {
+	    return createElement('span', {
 	      domProps: {
 	        innerHTML: outHtml
 	      }
